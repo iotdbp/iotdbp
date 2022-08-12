fbc0fa15999253581e1357e9ba9872ff90443a08
KhaledKanaan <khaled-kenaan@hotmail.com>
2022-08-12 09:39:03 +0300

    tester

--- script.js
+++ script.js
@@ -1,24 +1,66 @@
-$(function(){
+$(function () {
 
-    $(".ci").on('click',function() {
-
-        // var p_id = await getit(this);
+    $(".ci").on('click', function () {
 
         var id = this.id;
         var num = id.slice(3);
-        var p_tit = "tit_"+num;
-        var p_id = "#"+p_tit;
-      
-        console.log(p_id);
-
-        // $('html,body').animate({
-        //     scrollTop: $("#tit_2_3").offset().top
-        // }, 1000);
+        var p_tit = "tit_" + num;
+        var p_id = "#" + p_tit;
 
         $('html,body').animate({
             scrollTop: $(p_id).offset().top
-        }, 1000);
+        }, 800);
 
 
     });
+
+    var scrollTopFB = $("#scrollTopFB");
+    // When the user scrolls down 20px from the top of the document, show the button
+    //window.onscroll = function () { scrollFunction() };
+
+    $(window).on('scroll',scrollFunction());
+
+    
 });
+
+
+function scrollFunction() {
+    if ($('body').scrollTop > 20 || $(document.documentElement).scrollTop > 20) {
+        scrollTopFB.css("display", "block");
+    } else {
+        scrollTopFB.css("display", "none");
+    }
+}
+
+
+function topFunction() {
+    $(function () {
+        $('html,body').animate({
+            scrollTop: $(".main").offset().top
+        }, 800);
+    });
+}
+
+// When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// }
+
+// var scrollTopFB = document.getElementById("scrollTopFB");
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () { scrollFunction() };
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         scrollTopFB.style.display = "block";
+//     } else {
+//         scrollTopFB.style.display = "none";
+//     }
+// }
+
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// }
